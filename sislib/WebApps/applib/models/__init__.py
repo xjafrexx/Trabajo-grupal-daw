eee2b9bb867fed82fab56df4c9eb91f621f7ee36
AngelAN45 <aapazanaz@unsa.edu.pe>
2026-05-27 21:52:48 -0500

    estructurando lab06

---
+++ sislib/WebApps/applib/models/__init__.py
@@ -0,0 +1,13 @@
+from .book_copies import BookCopy
+from .books import Book
+from .categories import Category
+from .loans import Loan
+from .users import User
+
+__all__ = [
+    "BookCopy", 
+    "Book", 
+    "Category", 
+    "Loan", 
+    "User"
+]
