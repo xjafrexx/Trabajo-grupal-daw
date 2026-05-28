9298e81380ea24e932489c7bb1cfa78cb45fa41c
AngelAN45 <aapazanaz@unsa.edu.pe>
2026-05-27 22:53:49 -0500

    configurado

--- sislib/WebApps/applib/models/__init__.py
+++ sislib/WebApps/applib/models/__init__.py
@@ -1,13 +1,13 @@
-from .book_copies import BookCopy
 from .books import Book
+from .authors import Author
+from .authors_books import AuthorBook
+from .book_copies import BookCopy
+from .books_categories import BookCategory
 from .categories import Category
 from .loans import Loan
 from .users import User
 
 __all__ = [
-    "BookCopy", 
-    "Book", 
-    "Category", 
-    "Loan", 
-    "User"
+    "Book", "Author", "AuthorBook", "BookCopy", 
+    "BookCategory", "Category", "Loan", "User"
 ]
