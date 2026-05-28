53a54595aed3416e549b1aa6a4fbe047116b5437
AngelAN45 <aapazanaz@unsa.edu.pe>
2026-05-27 22:57:38 -0500

    configurado 1

--- sislib/WebApps/applib/admin.py
+++ sislib/WebApps/applib/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.books import Book
+from .models.authors import Author
+from .models.authors_books import AuthorBook
+from .models.book_copies import BookCopy
+from .models.books_categories import BookCategory
+from .models.categories import Category
+from .models.loans import Loan
+from .models.users import User
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(AuthorBook)
+admin.site.register(BookCopy)
+admin.site.register(BookCategory)
+admin.site.register(Category)
+admin.site.register(Loan)
+admin.site.register(User)
