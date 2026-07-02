04e80dcbb9dde4353d9a27293013b21bb36611c3
AngelAN45 <aapazanaz@unsa.edu.pe>
2026-07-01 23:19:28 -0500

    conectado a supabase

--- sislib/sislib/settings.py
+++ sislib/sislib/settings.py
@@ -12,9 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# --- INTEGRACIÓN DE TU GUÍA (CARGA DE VARIABLES) ---
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -77,9 +82,15 @@ WSGI_APPLICATION = 'sislib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('SUPABASE_DATABASE_HOST'),
+        'PORT': os.getenv('SUPABASE_DATABASE_PORT'),
+        'NAME': os.getenv('SUPABASE_DATABASE_NAME'),
+        'USER': os.getenv('SUPABASE_DATABASE_USER'),
+        'PASSWORD': os.getenv('SUPABASE_DATABASE_PASSWORD'),
+        #A veces es necesario incluir el certificado SSL si la conexión falla
+        'OPTIONS': {'sslmode': 'require'},
+    },
 }
 
 
